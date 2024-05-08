826cc6ad4323f0a3e3a494b287bb8e5ed59df4a6
Artur Costa <arturcostta@gmail.com>
2024-05-08 16:21:42 -0300

    Aulas de sql

---
+++ sql/transacao/transacao.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:password@/cursogo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	tx, _ := db.Begin()
+	stmt, _ := tx.Prepare("insert into usuarios(id,nome) values(?,?)")
+
+	stmt.Exec(2000, "Bia")
+	stmt.Exec(2001, "Carlos")
+	// _, err = stmt.Exec(1, "Tiago") // chave duplicada
+	_, err = stmt.Exec(4001, "Tiago") // chave duplicada
+
+	if err != nil {
+		tx.Rollback()
+		log.Fatal(err)
+	}
+
+	tx.Commit()
+}
